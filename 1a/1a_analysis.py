728f37b8cf623346a22914375039f96e548227a6
Bian Lee <bianlee2004@gmail.com>
2024-10-30 21:14:16 -0700

    .

--- 1a/1a_analysis.py
+++ 1a/1a_analysis.py
@@ -3,3 +3,8 @@ import dpkt
 filename='./HW1_1.pcap'
 f = open(filename, 'rb')
 pcap = dpkt.pcap.Reader(f)
+
+for timestamp, packet_data in pcap:
+    eth = dpkt.ethernet.Ethernet(packet_data)
+    ip = eth.data 
+    print(ip) # right now everything is in binary
