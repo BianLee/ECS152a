37dc4794dc70b7c3ec0f74ed4dd60674287a0e41
Bian Lee <bianlee2004@gmail.com>
2024-10-30 21:47:34 -0700

    6 iterations

--- 1a/1a_analysis.py
+++ 1a/1a_analysis.py
@@ -1,42 +1,76 @@
 import dpkt
 
-filename='./HW1_1.pcap'
-f = open(filename, 'rb') #open binary mode, if not there is error
-pcap = dpkt.pcap.Reader(f)
+pcap_files = ['./HW1_1.pcap', './HW1_2.pcap', './HW1_3.pcap', './HW1_4.pcap', './HW1_5.pcap', './HW1_6.pcap']
+
+for i in range(len(pcap_files)): #analyze all 6
+
+
+    filename = pcap_files[i]
+    f = open(filename, 'rb') #open binary mode, if not there is error
+    pcap = dpkt.pcap.Reader(f)
 
 
 
-#define the mapping of port number : protocol
-port_dictionary = {
-    80: "HTTP",
-    443: "HTTPS",
-    21: "FTP",
-    22: "SSH",
-    23: "Telnet",
-    25: "SMTP",
-    110: "POP",
-    53: "DNS",
-}
+    #define the mapping of port number : protocol
+    port_dictionary = {
+        80: "HTTP",
+        443: "HTTPS",
+        21: "FTP",
+        22: "SSH",
+        23: "Telnet",
+        25: "SMTP",
+        110: "POP",
+        53: "DNS",
+    }
 
-count_dictionary = dict()
-for key, value in port_dictionary.items(): #initializing all the protocol count to 0 first
-    count_dictionary[key] = 0
+    count_dictionary = dict()
+    for key, value in port_dictionary.items(): #initializing all the protocol count to 0 first
+        count_dictionary[key] = 0
 
-for timestamp, packet_data in pcap:
-    try:
-        eth = dpkt.ethernet.Ethernet(packet_data)
-        if isinstance(eth.data, dpkt.ip.IP): # case where it's an IP packet
-            ip = eth.data
+    for timestamp, packet_data in pcap:
+        try:
+            eth = dpkt.ethernet.Ethernet(packet_data)
+            if isinstance(eth.data, dpkt.ip.IP): # case where it's an IP packet
+                ip = eth.data
 
-            if isinstance(ip.data, dpkt.tcp.TCP): # if TCP
-                tcp = ip.data
-                if (tcp.sport in port_dictionary): #tcp.sport is source
-                    count_dictionary[tcp.sport]+=1
-                elif (tcp.dport in port_dictionary): #tcp.dport is destination
-                    count_dictionary[tcp.dport]+=1
-    except:
-        print("exception block, error")
+                if isinstance(ip.data, dpkt.tcp.TCP): # if TCP
+                    tcp = ip.data
+                    if (tcp.sport in port_dictionary): #tcp.sport is source
+                        count_dictionary[tcp.sport]+=1
+                    elif (tcp.dport in port_dictionary): #tcp.dport is destination
+                        count_dictionary[tcp.dport]+=1
+        except:
+            print("exception block, error")
 
 
-print(count_dictionary)
+    print(pcap_files[i] + ": ")
+    print(count_dictionary)
+    print("\n") #for formatting
 
+
+#output results
+'''
+./HW1_1.pcap: 
+{80: 0, 443: 60, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0}
+
+
+./HW1_2.pcap: 
+{80: 0, 443: 15, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0}
+
+
+./HW1_3.pcap: 
+{80: 0, 443: 0, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0}
+
+
+./HW1_4.pcap: 
+{80: 0, 443: 272, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0}
+
+
+./HW1_5.pcap: 
+{80: 0, 443: 5, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0}
+
+
+./HW1_6.pcap: 
+{80: 0, 443: 12, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0}
+
+'''
