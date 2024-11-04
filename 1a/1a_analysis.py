60e43033c637d63c49a717351a8145023cdafbf5
Bian Lee <bianlee2004@gmail.com>
2024-11-03 23:55:28 -0800

    new changes

--- 1a/1a_analysis.py
+++ 1a/1a_analysis.py
@@ -3,110 +3,79 @@ import socket
 
 pcap_files = ['./HW1_1.pcap', './HW1_2.pcap', './HW1_3.pcap', './HW1_4.pcap', './HW1_5.pcap', './HW1_6.pcap']
 
-for i in range(len(pcap_files)): #analyze all 6
+port_dictionary = {
+    80: "HTTP",
+    443: "HTTPS",
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    25: "SMTP",
+    110: "POP",
+    53: "DNS",
+    4500: "ESP",
+    5353: "MDNS",
+    1900: "SSDP"
+}
 
+application_protocols = {
+    "HTTP": True,
+    "HTTPS": True,
+    "FTP": True,
+    "Telnet": True,
+    "SMTP": True,
+    "POP": True,
+    "mDNS": True,
+    "SSDP": True,
+}
+
+class Count:
+    def __init__(self):
+        self.dict = {}
+        for port in port_dictionary:
+            self.dict[port_dictionary[port]] = 0
+        self.dict["ICMPv6"] = 0
+        self.dict["ARP"] = 0
+
+    def add_ports(self, src_port, dest_port):
+        if (src_port in port_dictionary):
+            self.dict[port_dictionary[src_port]]+=1
+        elif (dest_port in port_dictionary): 
+            self.dict[port_dictionary[dest_port]]+=1
+
+    def add(self, key):
+        if key in self.dict:
+            self.dict[key] += 1
+
+for i in range(len(pcap_files)): # analyze all 6
 
     filename = pcap_files[i]
-    f = open(filename, 'rb') #open binary mode, if not there is error
+    f = open(filename, 'rb') # open binary mode, if not there is error
     pcap = dpkt.pcap.Reader(f)
-
-    #define the mapping of port number : protocol
-    port_dictionary = {
-        80: "HTTP",
-        443: "HTTPS",
-        21: "FTP",
-        22: "SSH",
-        23: "Telnet",
-        25: "SMTP",
-        110: "POP",
-        53: "DNS",
-    }
-
-    count_dictionary = dict()
-    for key, value in port_dictionary.items(): #initializing all the protocol count to 0 first
-        count_dictionary[key] = 0
+    count = Count()
 
     for timestamp, packet_data in pcap:
         try:
             eth = dpkt.ethernet.Ethernet(packet_data)
             
-            if isinstance(eth.data, dpkt.ip.IP): # case where it's an IP packet
+            if isinstance(eth.data, dpkt.ip.IP):
                 ip = eth.data
-
-                if isinstance(ip.data, dpkt.tcp.TCP): # if TCP
-                    tcp = ip.data
-                    if (tcp.sport in port_dictionary): #tcp.sport is source
-                        count_dictionary[tcp.sport]+=1
-                    elif (tcp.dport in port_dictionary): #tcp.dport is destination
-                        count_dictionary[tcp.dport]+=1
-                
-                elif isinstance(ip.data, dpkt.udp.UDP):
-                    # print("hello")
-                    udp = ip.data
-              
-                    if (udp.sport in port_dictionary): #udp.sport is source
-                        # print('asdffsdaf')
-                        count_dictionary[udp.sport]+=1
-                    elif (udp.dport in port_dictionary): #udp.dport is destination
-                        # print('asdffsdaf')
-                        count_dictionary[udp.dport]+=1
-                
+                if isinstance(ip.data, dpkt.tcp.TCP) or isinstance(ip.data, dpkt.udp.UDP):
+                    count.add_ports(ip.data.sport, ip.data.dport)
+                        
             elif isinstance(eth.data, dpkt.ip6.IP6):
                 ip6 = eth.data
-
                 if isinstance(ip6.data, dpkt.icmp6.ICMP6):
-                    if ("ICMPv6" not in count_dictionary):
-                        count_dictionary["ICMPv6"] = 1 #if it doesn't exist just initialize
-                    else:
-                        count_dictionary["ICMPv6"] += 1
+                    count.add("ICMPv6")
+                elif isinstance(ip6.data, dpkt.udp.UDP) or isinstance(ip6.data, dpkt.tcp.TCP):
+                    count.add_ports(ip6.data.sport, ip6.data.dport)
                 
-                elif isinstance(ip6.data, dpkt.udp.UDP):
-                    udp = ip6.data
-                    if (udp.sport in port_dictionary):
-                        count_dictionary[udp.sport] += 1
-                    elif (udp.dport in port_dictionary): #should this be if or elif?
-                        count_dictionary[udp.dport] += 1 
-                
-                elif isinstance(ip6.data, dpkt.tcp.TCP):
-                    tcp = ip6.data
-                    if (tcp.sport in port_dictionary):
-                        count_dictionary[tcp.sport] += 1
-                    elif (tcp.dport in port_dictionary):  #should this be if or elif?
-                        count_dictionary[tcp.dport] += 1
-
-  
-                                
+            elif isinstance(eth.data, dpkt.arp.ARP):
+                count.add("ARP")
         except:
             print("exception block, error")
 
-
-    print(pcap_files[i] + ": ")
-    print(count_dictionary)
-    print("\n") #for formatting
-
-
-#output results
-'''
-./HW1_1.pcap: 
-{80: 0, 443: 89, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 12, 'ICMPv6': 50}
-
-
-./HW1_2.pcap: 
-{80: 0, 443: 24, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 4, 'ICMPv6': 3}
-
-
-./HW1_3.pcap: 
-{80: 0, 443: 0, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0}
-
-
-./HW1_4.pcap: 
-{80: 6, 443: 6125, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 214, 'ICMPv6': 8}
-
-
-./HW1_5.pcap: 
-{80: 0, 443: 8, 21: 22, 22: 0, 23: 0, 25: 0, 110: 0, 53: 0, 'ICMPv6': 4}
-
-
-./HW1_6.pcap: 
-{80: 0, 443: 86, 21: 0, 22: 0, 23: 0, 25: 0, 110: 0, 53: 28, 'ICMPv6': 8}
-'''
+    print(f"File {filename}")
+    for key in count.dict:
+        if key in application_protocols and count.dict[key] > 0:
+            print(f"{key.ljust(7)}: {count.dict[key]}")       
+    print()
